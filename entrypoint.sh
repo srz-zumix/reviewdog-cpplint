c7fff8bfb92abd96262a1950159793d65e24d558
srz_zumix <zumix.cpp@gmail.com>
2019-10-20 23:05:38 +0900

    add option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,10 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+REVIEWDOG_OPTION=
+if [ ${INPUT_REPORTER} = "local" ]; then
+    REVIEWDOG_OPTION='-diff "git diff"'
+fi
+
 cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
-  | reviewdog -efm="%f:%l: %m" -name="cpplint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -efm="%f:%l: %m" -name="cpplint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" $REVIEWDOG_OPTION
