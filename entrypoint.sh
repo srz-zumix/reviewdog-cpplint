e493201af6ebd7d2b8e8f6dfee535c7b38abee37
srz_zumix <zumix.cpp@gmail.com>
2019-10-21 13:58:26 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,15 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo ${INPUT_FLAGS}
-echo ${INPUT_TARGETS}
-echo ${INPUT_REPORTER}
-echo ${INPUT_LEVEL}
-echo ${INPUT_REVIEWDOG_OPTIONS}
-
-function reviewdog_cpplint() {
-    cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
-    | reviewdog -efm="%f:%l: %m" -name="cpplint" "$1" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${INPUT_REVIEWDOG_OPTIONS}"
-}
-
-if [ "${INPUT_REPORTER}" = "local" ]; then
-  reviewdog_cpplint '-diff=""git diff""'
-else
-  reviewdog_cpplint
+if [ -n "${INPUT_DEBUG}" ]; then
+    echo "cpplint option"
+    echo "  flags   : ${INPUT_FLAGS}"
+    echo "  targets : ${INPUT_TARGETS}"
+    echo "reviewdog option"
+    echo "  reporter: ${INPUT_REPORTER}"
+    echo "  level   : ${INPUT_LEVEL}"
+    echo "  options : ${INPUT_REVIEWDOG_OPTIONS}"
 fi
+
+cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
+  | reviewdog -efm="%f:%l: %m" -name="cpplint" -diff="git diff" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${INPUT_REVIEWDOG_OPTIONS}"
