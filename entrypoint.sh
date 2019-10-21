9928374419d0aa7ac0d726e147e4ebfe9a2a9b83
srz_zumix <zumix.cpp@gmail.com>
2019-10-21 10:14:15 +0900

    support local reporter

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,13 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-REVIEWDOG_OPTIONS=${IUTPUT_REVIEWDOG_OPTIONS:-""}
-if [ ${INPUT_REPORTER} = "local" ]; then
-    REVIEWDOG_OPTIONS+='-diff "git diff"'
-fi
+function reviewdog_cpplint() {
+    cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
+    | reviewdog -efm="%f:%l: %m" -name="cpplint" "$1" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${IUTPUT_REVIEWDOG_OPTIONS}"
+}
 
-cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
-  | reviewdog -efm="%f:%l: %m" -name="cpplint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" $REVIEWDOG_OPTIONS
+if [ "${INPUT_REPORTER}" = "local" ]; then
+  reviewdog_cpplint '-diff=""git diff""'
+else
+  reviewdog_cpplint
+fi
