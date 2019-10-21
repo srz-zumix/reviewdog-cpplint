2530af2963c4dc3b10b7d155006eecd2bda013f0
srz_zumix <zumix.cpp@gmail.com>
2019-10-21 23:45:37 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,13 @@ if [ -n "${INPUT_DEBUG}" ]; then
     echo "  options : ${INPUT_REVIEWDOG_OPTIONS}"
 fi
 
-cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
-  | reviewdog -efm="%f:%l: %m" -name="cpplint" -diff="git diff" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${INPUT_REVIEWDOG_OPTIONS}"
+function reviewdog_cpplint() {
+    cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
+    | reviewdog -efm="%f:%l: %m" -name="cpplint" "$1" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${INPUT_REVIEWDOG_OPTIONS}"
+}
+
+if [ "${INPUT_REPORTER}" = "local" ]; then
+  reviewdog_cpplint "-diff=""git diff origin/${GITHUB_BASE_REF}"""
+else
+  reviewdog_cpplint
+fi
