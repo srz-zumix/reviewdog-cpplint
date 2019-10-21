4434c56cf3f3df86d7c09f8be2346d1b076b2dae
srz_zumix <zumix.cpp@gmail.com>
2019-10-21 10:28:42 +0900

    test echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,15 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+echo ${INPUT_FLAGS}
+echo ${INPUT_TARGETS}
+echo ${INPUT_REPORTER}
+echo ${INPUT_LEVEL}
+echo ${INPUT_REVIEWDOG_OPTIONS}
+
 function reviewdog_cpplint() {
     cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
-    | reviewdog -efm="%f:%l: %m" -name="cpplint" "$1" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${IUTPUT_REVIEWDOG_OPTIONS}"
+    | reviewdog -efm="%f:%l: %m" -name="cpplint" "$1" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${INPUT_REVIEWDOG_OPTIONS}"
 }
 
 if [ "${INPUT_REPORTER}" = "local" ]; then
