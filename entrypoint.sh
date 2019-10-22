218d01079f419d9965d7951d1188c54e8db391f4
srz_zumix <zumix.cpp@gmail.com>
2019-10-22 10:19:30 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 if [ -n "${INPUT_DEBUG}" ]; then
-    # env
     echo "cpplint option"
     echo "  flags   : ${INPUT_FLAGS}"
     echo "  targets : ${INPUT_TARGETS}"
@@ -13,16 +12,9 @@ if [ -n "${INPUT_DEBUG}" ]; then
     echo "  reporter: ${INPUT_REPORTER}"
     echo "  level   : ${INPUT_LEVEL}"
     echo "  options : ${INPUT_REVIEWDOG_OPTIONS}"
-
-    # echo "cpplint: ${INPUT_FLAGS} ${INPUT_TARGETS}"
-    # cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1
 fi
 
 function reviewdog_cpplint() {
-    if [ -n "${INPUT_DEBUG}" ]; then
-        echo "cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
-          | reviewdog -efm="%f:%l: %m" -name="cpplint" "$1" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${INPUT_REVIEWDOG_OPTIONS}""
-    fi
     cpplint ${INPUT_FLAGS} ${INPUT_TARGETS} 2>&1 \
       | reviewdog -efm="%f:%l: %m" -name="cpplint" "$1" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" "${INPUT_REVIEWDOG_OPTIONS}"
 }
